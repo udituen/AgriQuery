f0ca52e7cee20a920165090882aacc3dfdb90a8a
udituen <udituen@gmail.com>
2025-07-18 18:09:20 -0600

    app update

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,11 +4,20 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to AgriQuery!
+### Under Development
 
 """
 
 st.title("AgriQuery - Agricultural AI Assistant")
 
 query = st.text_input("Ask a question about crops, soil, weather, etc.")
+
+# build my knowledge base and store in a vectore store - FAISS
+
+# initialise a LLM - Llama and send the vector along with query to LLM to prvide an output
+
+# system design 
+
+
+
 
